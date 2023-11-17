a329899c8214727a16518709ebed754cb2267219
10Nuts-Games <118637220+10Nuts-Games@users.noreply.github.com>
2023-11-17 08:20:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ function fruit(){
   window.location.href = 'https://10Nuts-Games.github.io/assets/games/fruit-ninja/';
 }
 
+function tetris(){
+  window.location.href = 'https://10nuts-games.github.io/assets/games/tetris/';
+}
+
 function ctr(){
   
 }
